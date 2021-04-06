d3f06f2e832e4101643ecbb339d1e1ce0e2f6dc2
florianvazelle <ponythugflorian@gmail.com>
2021-04-06 23:24:18 +0200

    A new start

---
+++ test/src/greeter.cpp
@@ -0,0 +1,15 @@
+#include <doctest.h>
+#include <greeter/greeter.hpp>
+
+#include <string>
+
+TEST_CASE("Greeter") {
+  using namespace greeter;
+
+  Greeter greeter("Tests");
+
+  CHECK(greeter.greet(LanguageCode::EN) == "Hello, Tests!");
+  CHECK(greeter.greet(LanguageCode::DE) == "Hallo Tests!");
+  CHECK(greeter.greet(LanguageCode::ES) == "¡Hola Tests!");
+  CHECK(greeter.greet(LanguageCode::FR) == "Bonjour Tests!");
+}
